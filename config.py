469a7589b40fe024c9e61ae7eb33b0732ea61e90
Azazelpvtgit <azazelfl4me@gmail.com>
2025-06-01 15:21:29 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+API_ID = 123456
+API_HASH = "your_api_hash"
+BOT_TOKEN = "your_bot_token"
+OWNER_ID = 123456789  # Your Telegram numeric ID
+
+MONGO_URL = "your_mongodb_uri"
+
+START_TEXT = "👋 Hello {user}, I'm here to auto-delete messages in your groups."
+START_IMAGE = "https://telegra.ph/file/fakeimage.jpg"  # or file_id
+
+SUPPORT_CHAT = "https://t.me/YourSupportGroup"
+UPDATES_CHANNEL = "https://t.me/YourUpdatesChannel"
