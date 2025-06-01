2bda5e676f5dc6a6f0f61f7f53a075f8a4115fee
Azazelpvtgit <azazelfl4me@gmail.com>
2025-06-01 17:11:10 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,60 +1,68 @@
 from motor.motor_asyncio import AsyncIOMotorClient
-from config import MONGO_URL
+import config
 
-mongo = AsyncIOMotorClient(MONGO_URL)
-db = mongo.autodeletebot
+client = AsyncIOMotorClient(config.MONGO_URL)
+db = client.autodel
 
-groups_col = db.groups
-users_col = db.users
-gbans_col = db.gbans
+users = db.users
+groups = db.groups
+gbans = db.gbans
 
-async def add_group(chat_id, title):
-    if not await groups_col.find_one({"chat_id": chat_id}):
-        await groups_col.insert_one({"chat_id": chat_id, "title": title})
-        # Set default timer to 24 hours (86400 sec)
-        await settings_col.update_one({"chat_id": chat_id}, {"$set": {
-            "timer": 86400,
-            "del_pinned": False,
-            "del_posts": True,
-            "del_comments": True
-        }}, upsert=True)
+
+async def add_user(user_id: int):
+    user = await users.find_one({"user_id": user_id})
+    if not user:
+        return await users.insert_one({"user_id": user_id})
+
+
+async def add_group(group_id: int):
+    group = await groups.find_one({"group_id": group_id})
+    if not group:
+        return await groups.insert_one({"group_id": group_id, "timer": 0})
+
+
 async def get_groups():
-    return await groups_col.find().to_list(length=None)
+    groups_list = []
+    async for group in groups.find():
+        groups_list.append(int(group["group_id"]))
+    return groups_list
 
-async def add_user(user_id):
-    await users_col.update_one({"user_id": user_id}, {"$set": {}}, upsert=True)
 
 async def get_users():
-    return await users_col.find().to_list(length=None)
+    users_list = []
+    async for user in users.find():
+        users_list.append(int(user["user_id"]))
+    return users_list
 
-async def set_timer(chat_id, hours):
-    await groups_col.update_one({"chat_id": chat_id}, {"$set": {"timer": hours}}, upsert=True)
 
-async def get_timer(chat_id):
-    data = await groups_col.find_one({"chat_id": chat_id})
-    return data.get("timer", None) if data else None
+async def get_timer(group_id: int):
+    group = await groups.find_one({"group_id": group_id})
+    if not group:
+        return 0
+    return group["timer"]
 
-async def set_setting(chat_id, key, value):
-    await groups_col.update_one({"chat_id": chat_id}, {"$set": {key: value}}, upsert=True)
 
-async def get_setting(chat_id, key):
-    data = await groups_col.find_one({"chat_id": chat_id})
-    return data.get(key, False) if data else False
+async def set_timer(group_id: int, timer: int):
+    await groups.update_one({"group_id": group_id}, {"$set": {"timer": timer}}, upsert=True)
 
-async def add_gban(user_id):
-    await gbans_col.update_one({"user_id": user_id}, {"$set": {}}, upsert=True)
 
-async def remove_gban(user_id):
-    await gbans_col.delete_one({"user_id": user_id})
+async def add_gban(user_id: int):
+    user = await gbans.find_one({"user_id": user_id})
+    if not user:
+        return await gbans.insert_one({"user_id": user_id})
 
-async def is_gbanned(user_id):
-    return await gbans_col.find_one({"user_id": user_id}) is not None
 
-async def add_sudo(user_id):
-    await db.sudos.update_one({"user_id": user_id}, {"$set": {}}, upsert=True)
+async def remove_gban(user_id: int):
+    return await gbans.delete_one({"user_id": user_id})
 
-async def get_sudos():
-    return [doc["user_id"] async for doc in db.sudos.find()]
-  async def get_gbans():
-    gbans = gbans_col.find()
-    return [u async for u in gbans]
+
+async def get_gbans():
+    banned_users = []
+    async for user in gbans.find():
+        banned_users.append(int(user["user_id"]))
+    return banned_users
+
+
+async def is_gbanned(user_id: int):
+    user = await gbans.find_one({"user_id": user_id})
+    return bool(user)
