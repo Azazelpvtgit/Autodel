917123b76a44a21e23c21ab188925521b1beebab
Azazelpvtgit <azazelfl4me@gmail.com>
2025-06-01 16:08:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ API_HASH = "5feffdf4111ed339381056d9476d3fcd"
 BOT_TOKEN = "7723361255:AAEgwawpNSLb5NyYQAEeTuzorwE6Cj9FWxY"
 OWNER_ID = 6061153252  # Your Telegram numeric ID
 
-MONGO_URL = "your_mongodb_uri"
+MONGO_URL = "mongodb+srv://txtfileleech:txtleech1234@cluster0.2az72uw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 START_TEXT = "👋 Hello {user}, I'm here to auto-delete messages in your groups. Powered by @Theodron Organization\n\n\n\ owner: @iceay"
 START_IMAGE = "https://envs.sh/s/GRC6Zsc5gQh9LJGhHhdiJw/E3w.jpg"  # or file_id
